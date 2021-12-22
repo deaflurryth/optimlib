29e8e278b0c265e77eb22a98688b01549c1e5728
Dmitrii Borodulin <54388816+deaflurryth@users.noreply.github.com>
2021-12-23 00:14:27 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
- 
+long_description= '''Python module for optimization tasks'''
 setup(name='optilib',
       version='0.1',
       url='https://github.com/deaflurryth/optimlib',
       license='MIT',
       author='deaflurryth',
-      author_email='amflurry@icloud.com',
+      author_email='deaflurryth@icloud.com',
       description='Add static script_dir() method to Path',
-      packages=find_packages(exclude=['tests']),
-      long_description=open('README.md').read(),
       zip_safe=False)
